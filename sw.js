4b5ffd7dca6819cbc7d8c75819371976b408adbb
Vill4zz <villapandocristopheraaron@gmail.com>
2026-09-12 12:23:36 +0800

    First commit

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'checklist-v1';
+const ASSETS = ['./', './index.html'];
+
+// Cache essential app files on install
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Serve cached app files when offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
